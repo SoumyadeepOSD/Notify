eacb06876fe9ca93134c30f91d8891cea093b3e5
SoumyadeepOSD <opencontainer255@gmail.com>
2023-06-18 22:25:45 +0530

    Project section done

--- src/Components/Projects/Project.jsx
+++ src/Components/Projects/Project.jsx
@@ -1,7 +1,39 @@
+import { SectionTitle } from "../GlobalComponent/GlobalComponent";
+import '../Projects/Project.css';
+
 const Projects = () => {
     return (
-      <div>Projects</div>
+      <div className="Container--project">
+        <SectionTitle title="Projects"/>
+        <div className="projects">
+          <ProjectCard/>
+          <ProjectCard/>
+          <ProjectCard/>
+          <ProjectCard/>
+          <ProjectCard/>
+          <ProjectCard/>
+          <ProjectCard/>
+          <ProjectCard/>
+          <ProjectCard/>
+          <ProjectCard/>
+          <ProjectCard/>
+          <ProjectCard/>
+        </div>
+      </div>
     )
   }
   
+
+  const ProjectCard = () => {
+    return(
+      <div className="Container--card">
+        <img src="https://rb.gy/q4ye0" alt="image" className="card--image"/>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni eveniet eum adipisci!</p>
+        <div className="card--bottom">
+          <button>Live</button>
+          <button>Code</button>
+        </div>
+      </div>
+    );
+  }
   export default Projects;
