c325d7ff6a90f155b03e5aa32c71f63772a44549
SoumyadeepOSD <opencontainer255@gmail.com>
2023-06-18 22:20:54 +0530

    Experiences Information added

---
+++ public/data.js
@@ -0,0 +1,38 @@
+export const data = 
+[
+    {
+        "id":1,
+        "Position":"Developer",
+        "Desc":"Best Developer",
+        "Start":"21.02.2021",
+        "End":"12.09.23"
+    },
+    {
+        "id":2,
+        "Position":"Developer",
+        "Desc":"Best Developer",
+        "Start":"21.02.2021",
+        "End":"12.09.23"
+    },
+    {
+        "id":3,
+        "Position":"Developer",
+        "Desc":"Best Developer",
+        "Start":"21.02.2021",
+        "End":"12.09.23"
+    },
+    {
+        "id":4,
+        "Position":"Developer",
+        "Desc":"Best Developer",
+        "Start":"21.02.2021",
+        "End":"12.09.23"
+    },
+    {
+        "id":5,
+        "Position":"Developer",
+        "Desc":"Best Developer",
+        "Start":"21.02.2021",
+        "End":"12.09.23"
+    }
+];
