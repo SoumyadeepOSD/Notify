75a56a5206bc071d6d11b21eb8ac60d489940e46
SoumyadeepOSD <opencontainer255@gmail.com>
2023-06-20 23:53:44 +0530

    Contact section done[1/2]

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,6 +1,18 @@
+import '../Contact/Contact.css';
+import {AiFillLinkedin, AiOutlineTwitter, AiOutlineGithub, AiFillYoutube} from 'react-icons/ai';
+import {SiBuymeacoffee, SiMedium, SiMastodon} from 'react-icons/si';
+
 const Contact = () => {
     return (
-      <div>Contact</div>
+      <div className="Container--contact">
+        <AiFillLinkedin/>
+        <AiOutlineTwitter/>
+        <AiOutlineGithub/>
+        <AiFillYoutube/>
+        <SiBuymeacoffee/>
+        <SiMedium/>
+        <SiMastodon/>
+      </div>
     )
   }
   
