ba2ff8cae61301bc2829da99ac5e1308ec756013
SoumyadeepOSD <opencontainer255@gmail.com>
2023-06-23 11:44:18 +0530

    Project details added

--- public/data.js
+++ public/data.js
@@ -1,38 +1,116 @@
-export const data = 
-[
-    {
-        "id":1,
-        "Position":"Developer",
-        "Desc":"Best Developer",
-        "Start":"21.02.2021",
-        "End":"12.09.23"
-    },
-    {
-        "id":2,
-        "Position":"Developer",
-        "Desc":"Best Developer",
-        "Start":"21.02.2021",
-        "End":"12.09.23"
-    },
-    {
-        "id":3,
-        "Position":"Developer",
-        "Desc":"Best Developer",
-        "Start":"21.02.2021",
-        "End":"12.09.23"
-    },
-    {
-        "id":4,
-        "Position":"Developer",
-        "Desc":"Best Developer",
-        "Start":"21.02.2021",
-        "End":"12.09.23"
-    },
-    {
-        "id":5,
-        "Position":"Developer",
-        "Desc":"Best Developer",
-        "Start":"21.02.2021",
-        "End":"12.09.23"
-    }
-];
+const data =
+    [
+        {
+            "id": 1,
+            "Position": "Developer",
+            "Desc": "Best Developer",
+            "Start": "21.02.2021",
+            "End": "12.09.23"
+        },
+        {
+            "id": 2,
+            "Position": "Developer",
+            "Desc": "Best Developer",
+            "Start": "21.02.2021",
+            "End": "12.09.23"
+        },
+        {
+            "id": 3,
+            "Position": "Developer",
+            "Desc": "Best Developer",
+            "Start": "21.02.2021",
+            "End": "12.09.23"
+        },
+        {
+            "id": 4,
+            "Position": "Developer",
+            "Desc": "Best Developer",
+            "Start": "21.02.2021",
+            "End": "12.09.23"
+        },
+        {
+            "id": 5,
+            "Position": "Developer",
+            "Desc": "Best Developer",
+            "Start": "21.02.2021",
+            "End": "12.09.23"
+        }
+    ];
+
+const projectData =
+    [
+        {
+            "id": 1,
+            "title": "Foody",
+            "url": "https://rb.gy/xbrlz",
+            "description": "The application offers an intuitive interface with visually appealing product displays, making it easy for users to find their desired items. Users can view product details, such as descriptions, prices, and images, enabling them to make informed purchasing decisions.",
+            "source": "",
+            "deploy": null,
+        },
+        {
+            "id": 2,
+            "title": "Quick-Chat",
+            "url": "https://rb.gy/xbrlz",
+            "description": "The application offers an intuitive interface with visually appealing product displays, making it easy for users to find their desired items. Users can view product details, such as descriptions, prices, and images, enabling them to make informed purchasing decisions.",
+            "source": "",
+            "deploy": null,
+        },
+        {
+            "id": 3,
+            "title": "Fast-News",
+            "url": "https://rb.gy/xbrlz",
+            "description": "The application offers an intuitive interface with visually appealing product displays, making it easy for users to find their desired items. Users can view product details, such as descriptions, prices, and images, enabling them to make informed purchasing decisions.",
+            "source": "",
+            "deploy": null,
+        },
+        {
+            "id": 4,
+            "title": "Weather-Visualizer",
+            "url": "https://rb.gy/xbrlz",
+            "description": "The application offers an intuitive interface with visually appealing product displays, making it easy for users to find their desired items. Users can view product details, such as descriptions, prices, and images, enabling them to make informed purchasing decisions.",
+            "source": "",
+            "deploy": null,
+        },
+        {
+            "id": 5,
+            "title": "Quick-Connect",
+            "url": "https://rb.gy/xbrlz",
+            "description": "The application offers an intuitive interface with visually appealing product displays, making it easy for users to find their desired items. Users can view product details, such as descriptions, prices, and images, enabling them to make informed purchasing decisions.",
+            "source": "",
+            "deploy": null,
+        },
+        {
+            "id": 6,
+            "title": "BookMyCart",
+            "url": "https://rb.gy/xbrlz",
+            "description": "The application offers an intuitive interface with visually appealing product displays, making it easy for users to find their desired items. Users can view product details, such as descriptions, prices, and images, enabling them to make informed purchasing decisions.",
+            "source": "",
+            "deploy": null,
+        },
+        {
+            "id": 7,
+            "title": "Velocity.io",
+            "url": "https://rb.gy/xbrlz",
+            "description": "The application offers an intuitive interface with visually appealing product displays, making it easy for users to find their desired items. Users can view product details, such as descriptions, prices, and images, enabling them to make informed purchasing decisions.",
+            "source": "",
+            "deploy": null,
+        },
+        {
+            "id": 8,
+            "title": "Job-Hunter",
+            "url": "https://rb.gy/xbrlz",
+            "description": "The application offers an intuitive interface with visually appealing product displays, making it easy for users to find their desired items. Users can view product details, such as descriptions, prices, and images, enabling them to make informed purchasing decisions.",
+            "source": "",
+            "deploy": null,
+        },
+        {
+            "id": 9,
+            "title": "MediCare",
+            "url": "https://rb.gy/xbrlz",
+            "description": "The application offers an intuitive interface with visually appealing product displays, making it easy for users to find their desired items. Users can view product details, such as descriptions, prices, and images, enabling them to make informed purchasing decisions.",
+            "source": "",
+            "deploy": null,
+        },
+    ];
+
+export { data, projectData };
