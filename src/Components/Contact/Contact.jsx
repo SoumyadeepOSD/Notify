f353a61143ee9ae5b0f69608625769363d3aa480
SoumyadeepOSD <opencontainer255@gmail.com>
2023-06-23 09:02:33 +0530

    Contact section done

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,19 +1,20 @@
 import '../Contact/Contact.css';
-import {AiFillLinkedin, AiOutlineTwitter, AiOutlineGithub, AiFillYoutube} from 'react-icons/ai';
-import {SiBuymeacoffee, SiMedium, SiMastodon} from 'react-icons/si';
+import { SectionTitle } from "../GlobalComponent/GlobalComponent";
+import { AiFillLinkedin, AiOutlineTwitter, AiOutlineGithub, AiFillYoutube } from 'react-icons/ai';
 
 const Contact = () => {
-    return (
-      <div className="Container--contact">
-        <AiFillLinkedin/>
-        <AiOutlineTwitter/>
-        <AiOutlineGithub/>
-        <AiFillYoutube/>
-        <SiBuymeacoffee/>
-        <SiMedium/>
-        <SiMastodon/>
+  return (
+    <div className='Container--contact'>
+      <SectionTitle title="Contact"/>
+      <div className="Social--icons">
+        <AiFillLinkedin size={30} />
+        <AiOutlineTwitter size={30} />
+        <AiOutlineGithub size={30} />
+        <AiFillYoutube size={30} />
       </div>
-    )
-  }
-  
-  export default Contact;
+      <p id='copyright'>Copyright © 2023 Notify, all right reserved</p>
+    </div>
+  )
+}
+
+export default Contact;
