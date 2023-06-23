9edc90033f5ac854283fe56c490d7d88c7f5826f
SoumyadeepOSD <opencontainer255@gmail.com>
2023-06-23 11:07:15 +0530

    Socials Links added

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -7,10 +7,18 @@ const Contact = () => {
       <div className='Container--contact' id='contact'>
         <SectionTitle title="Contact" />
         <div className="Social--icons">
+         <a href="https://www.linkedin.com/in/soumyadeep-das-bhowmick-01a882234/">
           <AiFillLinkedin size={30} />
+         </a>
+         <a href="https://twitter.com/SoumyadeepDasB6">
           <AiOutlineTwitter size={30} />
+         </a>
+         <a href="https://github.com/SoumyadeepOSD">
           <AiOutlineGithub size={30} />
+         </a>
+         <a href="https://youtube.com/@Open-Container">
           <AiFillYoutube size={30} />
+         </a>
         </div>
         <p id='copyright'>Copyright © 2023 Notify, all right reserved</p>
       </div>
