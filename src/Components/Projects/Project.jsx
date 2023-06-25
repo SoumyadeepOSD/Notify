4ec0f04cca3566a2d93bcbed4f115d5f77e84faa
SoumyadeepOSD <opencontainer255@gmail.com>
2023-06-25 22:42:34 +0530

    Minor changes in Project section

--- src/Components/Projects/Project.jsx
+++ src/Components/Projects/Project.jsx
@@ -1,6 +1,5 @@
 import { RevealX, SectionTitle } from "../GlobalComponent/GlobalComponent";
 import '../Projects/Project.css';
-import { motion } from "framer-motion";
 import { projectData } from "../../../public/data";
 
 
@@ -22,19 +21,23 @@ const ProjectCard = () => {
   return (
     projectData.map((e, i) => {
       return (
-        <motion.div
+        <div
           key={i}
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.9 }}
           className="Container--card">
           <img src={e.url} alt="image" className="card--image" />
           <h2 id="project--title">{e.title}</h2>
           <p>{e.description}</p>
           <div className="card--bottom">
-            <button><a className="btn" href={e.deploy}>Live</a></button>
-            <button><a className="btn" href={e.source}>Code</a></button>
+            <button 
+            className="btn"
+            onClick={()=>{location.href = e.deploy}}
+            >Live</button>
+            <button
+            className="btn"
+            onClick={()=>{location.href = e.source}}
+            >Code</button>
           </div>
-        </motion.div>
+        </div>
 
       );
     })
