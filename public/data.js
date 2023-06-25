6048fa825e258dc2f9e99b29198fac6808a4c7b6
SoumyadeepOSD <opencontainer255@gmail.com>
2023-06-25 22:41:29 +0530

    Personal data changes

--- public/data.js
+++ public/data.js
@@ -2,38 +2,35 @@ const data =
     [
         {
             "id": 1,
-            "Position": "Developer",
-            "Desc": "Best Developer",
-            "Start": "21.02.2021",
-            "End": "12.09.23"
+            "Position": "GetStream.io Hackathon",
+            "Desc": "Developed a chatting application using React and Stream Chat SDK",
+            "Start": "16.12.2022",
+            "End": "19.12.2022",
+            "Link":"https://devpost.com/software/community-chatroom"
         },
         {
             "id": 2,
-            "Position": "Developer",
-            "Desc": "Best Developer",
+            "Position": "Kavach Hackathon",
+            "Desc": "Made a security application that could detect the malicious messages and alert users from harmful activity in their mobile",
             "Start": "21.02.2021",
-            "End": "12.09.23"
+            "End": "12.09.23",
+            "Link":"https://drive.google.com/file/d/1Nqcb-n8I6IqSG8uUimyCbsum2b86qFXU/view?usp=sharing"
         },
         {
             "id": 3,
-            "Position": "Developer",
-            "Desc": "Best Developer",
+            "Position": "Google Solution Intra-College Hackathon",
+            "Desc": "Made a mobile application in flutter that could help people to solve the UN's #2nd goal 'Zero-Hunger'",
             "Start": "21.02.2021",
-            "End": "12.09.23"
+            "End": "12.09.23",
+            "Link":"https://github.com/SoumyadeepOSD/Hack_Hunger"
         },
         {
             "id": 4,
-            "Position": "Developer",
-            "Desc": "Best Developer",
+            "Position": "Hackathon Evangelist",
+            "Desc": "Evagelist for Hack4Bengal to \n promote Hackathon culture among folks",
             "Start": "21.02.2021",
-            "End": "12.09.23"
-        },
-        {
-            "id": 5,
-            "Position": "Developer",
-            "Desc": "Best Developer",
-            "Start": "21.02.2021",
-            "End": "12.09.23"
+            "End": "12.09.23",
+            "Link":"https://twitter.com/SoumyadeepDasB6/status/1641279605888917505?s=20"
         }
     ];
 
@@ -41,33 +38,33 @@ const projectData =
     [
         {
             "id": 1,
-            "title": "Foody",
-            "url": "https://rb.gy/xbrlz",
-            "description": "The application offers an intuitive interface with visually appealing product displays, making it easy for users to find their desired items. Users can view product details, such as descriptions, prices, and images, enabling them to make informed purchasing decisions.",
-            "source": "https://github.com/SoumyadeepOSD/Foody",
+            "title": "HackHunger",
+            "url": "https://www150.statcan.gc.ca/n1/pub/11-637-x/img/goal02_icon-eng.jpg",
+            "description": "This application is made to solve the 2nd sustainable united nation problem called 'Zero Hunger', by different kind of donation techniques people will able to donate monitory as well as food support to the victims",
+            "source": "https://github.com/SoumyadeepOSD/Hack_Hunger",
             "deploy": null,
         },
         {
             "id": 2,
             "title": "Quick-Chat",
-            "url": "https://rb.gy/xbrlz",
-            "description": "The application offers an intuitive interface with visually appealing product displays, making it easy for users to find their desired items. Users can view product details, such as descriptions, prices, and images, enabling them to make informed purchasing decisions.",
+            "url": "https://i.pinimg.com/originals/68/a6/70/68a670b78e9c71abb06b1a14ebb44afc.jpg",
+            "description": "Quick Chat is an Socket-IO based chat application that you can use to chat among peers who are in the same network and can communicate between them also",
             "source": "https://github.com/SoumyadeepOSD/NodeJs-ChatApp",
             "deploy": null,
         },
         {
             "id": 3,
-            "title": "Fast-News",
-            "url": "https://rb.gy/xbrlz",
-            "description": "The application offers an intuitive interface with visually appealing product displays, making it easy for users to find their desired items. Users can view product details, such as descriptions, prices, and images, enabling them to make informed purchasing decisions.",
-            "source": "",
+            "title": "Scrapy-Tube",
+            "url": "https://th.bing.com/th/id/R.320d8652a3450d354527beb88fb0cc37?rik=ZZ1K2EjWqAj9YQ&riu=http%3a%2f%2fmediad.publicbroadcasting.net%2fp%2fkcbx%2ffiles%2fstyles%2fx_large%2fpublic%2f201610%2fscrap_logo-med_cmyk.jpg&ehk=Iie%2bdANGmsQVi6jWYMv1bG0BPcJR%2fy3M43TLr%2fNYfys%3d&risl=&pid=ImgRaw&r=0",
+            "description": "Youtube contains lots of comments for a particular video and this app allows us to fetch all the comments and analyse the most frequence once",
+            "source": "https://github.com/SoumyadeepOSD/Youtube_Comment_Scrapper",
             "deploy": null,
         },
         {
             "id": 4,
             "title": "Weather-Visualizer",
-            "url": "https://rb.gy/xbrlz",
-            "description": "The application offers an intuitive interface with visually appealing product displays, making it easy for users to find their desired items. Users can view product details, such as descriptions, prices, and images, enabling them to make informed purchasing decisions.",
+            "url": "https://th.bing.com/th/id/OIP.ueV4jMS9VqVehmZ8gaH4XQHaHI?pid=ImgDet&rs=1",
+            "description": "It's a weather application made with free Weather API. Depending upon the Current weather scenario for a particular place it returns back the reponses in the terms of symbols and temperatures",
             "source": "https://github.com/SoumyadeepOSD/Weather_App",
             "deploy": null,
         },
